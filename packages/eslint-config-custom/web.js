5ff483e0cc722d7ab9924ff7e335d20fd0f96cf8
Alexis Le Boucher <alexis.le-boucher@osedea.com>
2023-09-20 17:53:18 +0200

    Improved eslint config for web

--- packages/eslint-config-custom/web.js
+++ packages/eslint-config-custom/web.js
@@ -4,7 +4,7 @@ module.exports = {
     project: './apps/web/tsconfig.json',
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       typescript: {
         project: './apps/web/tsconfig.json',
       },
@@ -12,14 +12,59 @@ module.exports = {
   },
   env: { browser: true, es2020: true },
   extends: [
-    // Find a way to add 'eslint:recommended'
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:eslint-comments/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    "plugin:react/recommended",
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
-  plugins: ['react-refresh'],
+  plugins: ['react', 'react-refresh', '@arthurgeron/react-usememo'],
   rules: {
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'spaced-comment': 'warn',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-duplicate-imports': 'off',
+    'jsx-quotes': ['error', 'prefer-double'],
+    'no-multiple-empty-lines': ['error', { max: 1 }],
+    'newline-before-return': 'error',
+    'camelcase': 'warn',
+    'object-curly-spacing': ['error', 'always'],
+    'spaced-comment': 'warn',
+    'require-await': 'off',
+    'arrow-body-style': ['error', 'as-needed'],
+    'eqeqeq': 'error',
+
+    '@typescript-eslint/await-thenable': 'error',
+    '@typescript-eslint/require-await': 'error',
+    '@typescript-eslint/no-unnecessary-condition': 'warn',
+    '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
+
+    'eslint-comments/no-unused-disable': 'error',
+
+    'import/no-duplicates': ['error'],
+    'import/order': ['error', {
+      groups: [
+        ['builtin', 'external'],
+        ['internal', 'index', 'sibling', 'parent', 'object']
+      ],
+      'newlines-between': 'always-and-inside-groups'
+    }],
+
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/no-array-index-key': 'warn',
+    'react/jsx-key': 'error',
+    'react/jsx-no-useless-fragment': 'error',
+    'react/self-closing-comp': ['error', { component: true, html: true }],
+    'react/jsx-no-constructed-context-values': 'error',
+
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+
+    '@arthurgeron/react-usememo/require-usememo': ['error', { checkHookReturnObject: true }],
   },
 }
