48d3b3289e2e32c620d2c80fdbbed036832c989d
alexleboucher <dev.alexisleboucher@gmail.com>
2023-10-14 12:23:12 +0200

    Improved code and config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const { HOST, WEB_PORT = 3000 } = process.env;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: HOST,
+    port: +WEB_PORT,
     watch: {
       usePolling: true,
     },
